0042d06c13901ff89ada2e3ae517fc6d7f82c7c4
박민호 <radlohead@naver.com>
2017-01-31 14:41:11 +0900

    react init

--- react_init/webpack.config.js
+++ react_init/webpack.config.js
@@ -1,25 +1,21 @@
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: __dirname + '/output',
-        filename: 'bundle.js'
+        path: __dirname + '/output', //번들 파일 폴더
+        filename: 'bundle.js' //번들 파일 이름
     },
     devServer: {
-        inline: true,
-        port: 8080,
+        inline: true, //자동 리로드 여부를 선택합니다.
+        port: 8080, //로컬 서버주소를 설정합니다.
         contentBase: __dirname + '/output'
     },
     module:
     {
         loaders: [
             {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/,
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'stage-0', 'react']
-                }
+                test: /\.js$/, //로더를 사용할 확장자를 추가합니다.
+                loader: 'babel', //로더를 설정합니다.
+                exclude: /node_modules/,  //로더 사용 제외한 대상을 추가합니다.
             }
         ]
     }
